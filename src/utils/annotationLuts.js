def67c3de3df3f5022efdaa412e672fb896c7f95
jmdowrick <146781982+jmdowrick@users.noreply.github.com>
2026-03-17 15:56:25 +1300

    Add chemical flow rate to the annotation look up table

--- src/utils/annotationLuts.js
+++ src/utils/annotationLuts.js
@@ -1,4 +1,4 @@
 // Store look up tables for annotations
-export const CHEBI_NAMES = { 'chebi:4167': 'glucose', 'chebi:29101': 'Na+' }
-export const FMA_NAMES   = { 'fma:66836': 'cytosol',  'fma:9672': 'ECM' }
-export const OPB_NAMES   = { 'opb:00378': 'potential', 'opb:00425': 'quantity' }
+export const CHEBI_NAMES = { 'chebi:4167': 'glucose', 'chebi:29101': 'Na+', }
+export const FMA_NAMES   = { 'fma:66836': 'cytosol',  'fma:9672': 'ECM', }
+export const OPB_NAMES   = { 'opb:00378': 'potential', 'opb:00425': 'quantity', 'opb:00592': 'chemical quantity flow rate'}
